23f326312188f00a053cf91f3ed4eeee50e847f2
gabrielf-andrade <gabrielf_andrade@hotmail.com>
2025-07-21 02:37:42 -0300

    chore: update tsconfig to include next.config.mjs and add wrangler configuration file

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -4,6 +4,8 @@ import ProjectsSection from "@/components/sections/projects-section";
 import { SkillsSection } from "@/components/sections/skills-section";
 import { Separator } from "@/components/ui/separator";
 
+export const runtime = "edge";
+
 export default async function HomePage() {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
